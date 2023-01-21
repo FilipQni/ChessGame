4d60a2bf1d7a454229b21024eb1edd05211b4a84
Filip <qnickicoding@gmail.com>
2023-01-21 16:36:02 +0100

    Add color checker method Remove coordinates parameter from the constructor Change access modifier of the isBlack field

--- src/chessGame/pieces/Piece.java
+++ src/chessGame/pieces/Piece.java
@@ -1,28 +1,14 @@
 package chessGame.pieces;
-import javax.imageio.ImageIO;
-import java.awt.Point;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Piece {
-    private Point coordinates;
-    private final boolean isBlack;
+    protected final boolean isBlack;
 
-    public Piece(boolean isBlack, Point coordinates) {
-        this.coordinates = coordinates;
+    public Piece(boolean isBlack) {
         this.isBlack = isBlack;
     }
 
-    public BufferedImage loadPieceImage(){
-        BufferedImage img = null;
-        try {
-            System.out.println("Present Project Directory : "+ System.getProperty("user.dir"));
-            img = ImageIO.read(new File("../src/images/test.png"));
-        } catch (IOException e) {
-            System.out.println("Could not load a piece image");
-        }
-        return img;
+    public boolean isBlack() {
+        return isBlack;
     }
 
 }
