0b1bf94f8133846a93b1bc9f0ba946445cc61a39
Filip <qnickicoding@gmail.com>
2023-01-21 16:37:38 +0100

    Add setPiece method Add constructor

--- src/chessGame/Square.java
+++ src/chessGame/Square.java
@@ -2,7 +2,11 @@ package chessGame;
 
 import chessGame.pieces.Piece;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Square extends JButton {
 
@@ -10,5 +14,25 @@ public class Square extends JButton {
 
     public Square(Piece piece) {
         this.piece = piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+        BufferedImage pieceImage = null;
+
+        char colorFirstLetter;
+        String fileExtension = ".png";
+        if(this.piece.isBlack()) colorFirstLetter = 'b';
+        else colorFirstLetter = 'w';
+
+        String imagePathname = "../src/images/" + colorFirstLetter + this.piece.getClass().getSimpleName() + fileExtension;
+
+        try {
+            pieceImage = ImageIO.read(new File(imagePathname));
+        } catch (IOException e) {
+            System.out.println("Could not load a piece image");
+        }
+
+        this.setIcon(new ImageIcon(pieceImage));
     }
 }
