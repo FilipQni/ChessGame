753447c1c043fdff3c37cd8e4b0ceaf6ce1e39a4
Filip <qnickicoding@gmail.com>
2023-01-22 18:18:16 +0100

    Add getCoordinates(), isEmpty(), isPieceBlack()

--- src/chessGame/Square.java
+++ src/chessGame/Square.java
@@ -4,18 +4,29 @@ import chessGame.pieces.Piece;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Square extends JButton {
-
+public class Square extends JButton{
+    private Point coordinates;
     private Piece piece;
-
-    public Square(Piece piece) {
+    public Square(Piece piece, Point coordinates) {
         this.piece = piece;
+        this.coordinates = coordinates;
     }
-
+    public Point getCoordinates() {
+        return coordinates;
+    }
+    public Boolean isPieceBlack(){
+        if (!isEmpty()) return piece.isBlack();
+        return null;
+    }
+    public boolean isEmpty(){
+        if (this.piece == null) return true;
+        return false;
+    }
     public void setPiece(Piece piece) {
         this.piece = piece;
         BufferedImage pieceImage = null;
@@ -35,4 +46,6 @@ public class Square extends JButton {
 
         this.setIcon(new ImageIcon(pieceImage));
     }
+
+
 }
