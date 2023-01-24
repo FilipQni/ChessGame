218091cbe0511b8c628e1c5fd886e5e1d4a80db7
Filip <qnickicoding@gmail.com>
2023-01-24 12:38:24 +0100

    Add isPositionOnTheBoard() and getPossibleMoves()

--- src/chessGame/pieces/Piece.java
+++ src/chessGame/pieces/Piece.java
@@ -1,5 +1,10 @@
 package chessGame.pieces;
 
+import chessGame.Square;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Piece {
     protected final boolean isBlack;
 
@@ -9,6 +14,14 @@ public class Piece {
 
     public boolean isBlack() {
         return isBlack;
+    }
+
+    public ArrayList<Point> getPossibleMoves(Square board[][], Point coordinates) {
+        return new ArrayList<Point>();
+    }
+
+    public boolean isPositionOnTheBoard(Point coordinates) {
+        return ((coordinates.x >= 0 && coordinates.x <= 7) && (coordinates.y >= 0 && coordinates.y <= 7));
     }
 
 }
