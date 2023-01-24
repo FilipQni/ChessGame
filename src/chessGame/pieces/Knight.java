007ab2c01637d3fd9fdffa1a8f9444e21ff2d2f5
Filip <qnickicoding@gmail.com>
2023-01-24 12:38:59 +0100

    Add getPossibleMoves() to the Knight class

--- src/chessGame/pieces/Knight.java
+++ src/chessGame/pieces/Knight.java
@@ -1,7 +1,31 @@
 package chessGame.pieces;
 
+import chessGame.Square;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Knight extends Piece {
     public Knight(boolean isBlack) {
         super(isBlack);
+    }
+
+    @Override
+    public ArrayList<Point> getPossibleMoves(Square[][] board, Point coordinates) {
+        ArrayList<Point> possibleMoves = new ArrayList<>();
+        Point nextPosition = new Point();
+        int[] X = {-2, -2, -1, -1, 1, 1, 2, 2};
+        int[] Y = {-1, 1, -2, 2, 2, -2, 1, -1};
+
+        for (int i = 0; i < 8; i++) {
+            nextPosition.x = coordinates.x + X[i];
+            nextPosition.y = coordinates.y + Y[i];
+            if (isPositionOnTheBoard(nextPosition)) {
+                if (board[nextPosition.x][nextPosition.y].isEmpty() || isBlack != board[nextPosition.x][nextPosition.y].isPieceBlack()) {
+                    possibleMoves.add(new Point(nextPosition.x , nextPosition.y));
+                }
+            }
+        }
+        return possibleMoves;
     }
 }
