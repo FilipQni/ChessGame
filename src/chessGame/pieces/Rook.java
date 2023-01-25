23b3bb1a4770924a7e2958c03bba4ba7bae12fba
Filip <qnickicoding@gmail.com>
2023-01-25 13:46:16 +0100

    Add Rook movement

--- src/chessGame/pieces/Rook.java
+++ src/chessGame/pieces/Rook.java
@@ -1,7 +1,51 @@
 package chessGame.pieces;
 
+import chessGame.Square;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Rook extends Piece{
     public Rook(boolean isBlack) {
         super(isBlack);
+    }
+
+    @Override
+    public ArrayList<Point> getPossibleMoves(Square[][] board, Point coordinates) {
+        ArrayList<Point> possibleMoves = new ArrayList<>();
+        Point positionTocheck = coordinates;
+
+        //up
+        positionTocheck.y++;
+        while (this.isPositionOnTheBoard(positionTocheck)){
+            positionTocheck.y++;
+            possibleMoves.add(positionTocheck);
+        }
+
+        //down
+        positionTocheck = coordinates;
+        positionTocheck.y--;
+        while (this.isPositionOnTheBoard(positionTocheck)){
+            positionTocheck.y--;
+            possibleMoves.add(positionTocheck);
+        }
+
+        //left
+        positionTocheck = coordinates;
+        positionTocheck.x--;
+        while (this.isPositionOnTheBoard(positionTocheck)){
+            positionTocheck.x--;
+            possibleMoves.add(positionTocheck);
+        }
+
+        //right
+        positionTocheck = coordinates;
+        positionTocheck.x++;
+        while (this.isPositionOnTheBoard(positionTocheck)){
+            positionTocheck.x++;
+            possibleMoves.add(positionTocheck);
+        }
+
+        return possibleMoves;
     }
 }
