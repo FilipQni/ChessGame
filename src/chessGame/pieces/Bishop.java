2593ebe6f984c7340f6e4469becc53613cec8399
Filip <qnickicoding@gmail.com>
2023-01-30 00:46:17 +0100

    Add Bishop movement

--- src/chessGame/pieces/Bishop.java
+++ src/chessGame/pieces/Bishop.java
@@ -1,7 +1,82 @@
 package chessGame.pieces;
 
+import chessGame.Square;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Bishop extends Piece {
     public Bishop(boolean isBlack) {
         super(isBlack);
+    }
+    @Override
+    public ArrayList<Point> getPossibleMoves(Square[][] board, Point coordinates) {
+        ArrayList<Point> possibleMoves = new ArrayList<>();
+        Point possibleMove = new Point(coordinates);
+
+        //right up
+        possibleMove.x++;
+        possibleMove.y++;
+        while (this.isPositionOnTheBoard(possibleMove)){
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.x++;
+            possibleMove.y++;
+        }
+
+        //left up
+        possibleMove.setLocation(coordinates);
+        possibleMove.x--;
+        possibleMove.y++;
+        while (this.isPositionOnTheBoard(possibleMove)){
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.x--;
+            possibleMove.y++;
+        }
+
+        //left down
+        possibleMove.setLocation(coordinates);
+        possibleMove.x--;
+        possibleMove.y--;
+        while (this.isPositionOnTheBoard(possibleMove)){
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.x--;
+            possibleMove.y--;
+        }
+
+        //right down
+        possibleMove.setLocation(coordinates);
+        possibleMove.x++;
+        possibleMove.y--;
+        while (this.isPositionOnTheBoard(possibleMove)){
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.x++;
+            possibleMove.y--;
+        }
+
+        return possibleMoves;
     }
 }
