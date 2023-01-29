50ffa2e6621447a2d449f13832191b8e434ab577
Filip <qnickicoding@gmail.com>
2023-01-30 00:47:08 +0100

    Fix Rook movement

--- src/chessGame/pieces/Rook.java
+++ src/chessGame/pieces/Rook.java
@@ -5,7 +5,7 @@ import chessGame.Square;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class Rook extends Piece{
+public class Rook extends Piece {
     public Rook(boolean isBlack) {
         super(isBlack);
     }
@@ -13,37 +13,61 @@ public class Rook extends Piece{
     @Override
     public ArrayList<Point> getPossibleMoves(Square[][] board, Point coordinates) {
         ArrayList<Point> possibleMoves = new ArrayList<>();
-        Point positionTocheck = coordinates;
+        Point possibleMove = new Point(coordinates);
 
         //up
-        positionTocheck.y++;
-        while (this.isPositionOnTheBoard(positionTocheck)){
-            positionTocheck.y++;
-            possibleMoves.add(positionTocheck);
+        possibleMove.y++;
+        while (this.isPositionOnTheBoard(possibleMove)) {
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.y++;
         }
 
         //down
-        positionTocheck = coordinates;
-        positionTocheck.y--;
-        while (this.isPositionOnTheBoard(positionTocheck)){
-            positionTocheck.y--;
-            possibleMoves.add(positionTocheck);
+        possibleMove.setLocation(coordinates);
+        possibleMove.y--;
+        while (this.isPositionOnTheBoard(possibleMove)) {
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.y--;
         }
 
         //left
-        positionTocheck = coordinates;
-        positionTocheck.x--;
-        while (this.isPositionOnTheBoard(positionTocheck)){
-            positionTocheck.x--;
-            possibleMoves.add(positionTocheck);
+        possibleMove.setLocation(coordinates);
+        possibleMove.x--;
+        while (this.isPositionOnTheBoard(possibleMove)) {
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.x--;
         }
 
         //right
-        positionTocheck = coordinates;
-        positionTocheck.x++;
-        while (this.isPositionOnTheBoard(positionTocheck)){
-            positionTocheck.x++;
-            possibleMoves.add(positionTocheck);
+        possibleMove.setLocation(coordinates);
+        possibleMove.x++;
+        while (this.isPositionOnTheBoard(possibleMove)) {
+            if (!board[possibleMove.x][possibleMove.y].isEmpty()) {
+                if (this.isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack())
+                    possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+                break;
+            }
+
+            possibleMoves.add(new Point(possibleMove.x, possibleMove.y));
+            possibleMove.x++;
         }
 
         return possibleMoves;
