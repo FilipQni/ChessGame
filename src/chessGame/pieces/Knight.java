9736bd33195496a591016cdddfd204aef4f9d1cc
Filip <qnickicoding@gmail.com>
2023-01-30 00:50:34 +0100

    Fix Knight movement

--- src/chessGame/pieces/Knight.java
+++ src/chessGame/pieces/Knight.java
@@ -13,16 +13,16 @@ public class Knight extends Piece {
     @Override
     public ArrayList<Point> getPossibleMoves(Square[][] board, Point coordinates) {
         ArrayList<Point> possibleMoves = new ArrayList<>();
-        Point nextPosition = new Point();
+        Point possibleMove = new Point();
         int[] X = {-2, -2, -1, -1, 1, 1, 2, 2};
         int[] Y = {-1, 1, -2, 2, 2, -2, 1, -1};
 
         for (int i = 0; i < 8; i++) {
-            nextPosition.x = coordinates.x + X[i];
-            nextPosition.y = coordinates.y + Y[i];
-            if (isPositionOnTheBoard(nextPosition)) {
-                if (board[nextPosition.x][nextPosition.y].isEmpty() || isBlack != board[nextPosition.x][nextPosition.y].isPieceBlack()) {
-                    possibleMoves.add(new Point(nextPosition.x , nextPosition.y));
+            possibleMove.x = coordinates.x + X[i];
+            possibleMove.y = coordinates.y + Y[i];
+            if (isPositionOnTheBoard(possibleMove)) {
+                if (board[possibleMove.x][possibleMove.y].isEmpty() || isBlack != board[possibleMove.x][possibleMove.y].isPieceBlack()) {
+                    possibleMoves.add(new Point(possibleMove.x , possibleMove.y));
                 }
             }
         }
