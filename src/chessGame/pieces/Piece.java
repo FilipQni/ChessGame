d292522a186f634e48ffd8f9192e20995c577e0b
Filip <qnickicoding@gmail.com>
2023-01-30 00:49:08 +0100

    Add setHasMoved() method

--- src/chessGame/pieces/Piece.java
+++ src/chessGame/pieces/Piece.java
@@ -7,21 +7,26 @@ import java.util.ArrayList;
 
 public class Piece {
     protected final boolean isBlack;
-
+    protected boolean hasMoved;
     public Piece(boolean isBlack) {
         this.isBlack = isBlack;
+        this.hasMoved = false;
     }
 
     public boolean isBlack() {
         return isBlack;
     }
 
-    public ArrayList<Point> getPossibleMoves(Square board[][], Point coordinates) {
+    public ArrayList<Point> getPossibleMoves(Square[][] board, Point coordinates) {
         return new ArrayList<Point>();
     }
 
     public boolean isPositionOnTheBoard(Point coordinates) {
         return ((coordinates.x >= 0 && coordinates.x <= 7) && (coordinates.y >= 0 && coordinates.y <= 7));
+    }
+
+    public void setHasMoved(){
+        this.hasMoved = true;
     }
 
 }
