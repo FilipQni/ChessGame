bbb596405268a88ab4cf12215ad43fb62d9110a6
Filip <qnickicoding@gmail.com>
2023-02-20 11:10:11 +0100

    Add move method and coordinates field

--- src/chessGame/pieces/Piece.java
+++ src/chessGame/pieces/Piece.java
@@ -3,30 +3,43 @@ package chessGame.pieces;
 import chessGame.Square;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Piece {
     protected final boolean isBlack;
     protected boolean hasMoved;
-    public Piece(boolean isBlack) {
+    protected Point coordinates;
+
+    public Piece(boolean isBlack, Point coordinates) {
         this.isBlack = isBlack;
         this.hasMoved = false;
+        this.coordinates = coordinates;
     }
 
     public boolean isBlack() {
         return isBlack;
     }
 
-    public ArrayList<Point> getPossibleMoves(Square[][] board, Point coordinates) {
-        return new ArrayList<Point>();
+    public boolean getHasMoved() {
+        return hasMoved;
+    }
+
+    public List<Point> getPossibleMoves(Square[][] board) {
+        return new LinkedList<>();
     }
 
     public boolean isPositionOnTheBoard(Point coordinates) {
         return ((coordinates.x >= 0 && coordinates.x <= 7) && (coordinates.y >= 0 && coordinates.y <= 7));
     }
 
-    public void setHasMoved(){
+    public void setHasMoved() {
         this.hasMoved = true;
+    }
+
+    public void move(Square[][] board, Point coordinates){
+        board[coordinates.x][coordinates.y].setPiece(this);
+        this.coordinates = coordinates;
     }
 
 }
